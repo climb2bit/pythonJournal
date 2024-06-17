917be21b374532e9cbd27b1a784951ecee5be9f2
climb2bit <rhorizonus@hotmail.com>
2024-06-17 20:56:24 +0800

    dance~!

--- DKBook2/dance.py
+++ DKBook2/dance.py
@@ -1,5 +1,6 @@
 import pgzrun
 from random import randint
+import time
 
 WIDTH = 800
 HEIGHT = 600
@@ -50,6 +51,13 @@ def draw():
         if show_countdown:
             screen.draw.text(str(count), color = "black",
                              topleft = (CENTER_X - 8, 150), fontsize = 60)
+        
+    else:
+        screen.clear()
+        screen.blit("stage", (0,0))
+        screen.draw.text("Score: " + str(score), color = "black",
+                         topleft = (10,10))
+        screen.draw.text("GAME OVER!", color = "black", topleft = (CENTER_X - 130,220), fontsize = 60)
     return
 
 def reset_dancer():
@@ -90,16 +98,16 @@ def display_moves():
         this_move = display_list[0]
         display_list = display_list[1:]
         if this_move == 0:
-            update_dancer[0]
+            update_dancer(0)
             clock.schedule(display_moves, 1)
         elif this_move == 1:
-            update_dancer[1]
+            update_dancer(1)
             clock.schedule(display_moves, 1)
         elif this_move == 2:
-            update_dancer[2]
+            update_dancer(2)
             clock.schedule(display_moves, 1)
         else:
-            update_dancer[3]
+            update_dancer(3)
             clock.schedule(display_moves, 1)
     else:
         say_dance = True
@@ -108,34 +116,81 @@ def display_moves():
 
 
 def generate_moves():
-    pass
+    global move_list, dance_length, count
+    global show_countdown, say_dance
+    count = 4
+    move_list = []
+    say_dance = False
+    for move in range(0, dance_length):
+        rand_move = randint(0,3)
+        move_list.append(rand_move)
+        display_list.append(rand_move)
+    show_countdown = True    
+    print (display_list)
+    countdown()    
+    return
 
 def countdown():
-    global count, game_over, show_countdown
+    global count, game_over, show_countdown, say_dance
     if count > 1:
         count = count-1
+        #print (f'[DEBUG] {count}')      
         clock.schedule(countdown, 1)
-    else:
+    elif say_dance == False:
         show_countdown = False
         display_moves()
     return
 
+
 def next_move():
-    pass
+    global dance_length, current_move, moves_complete
+    if current_move < dance_length -1:
+        current_move = current_move +1
+    else:
+        moves_complete = True
+    return
 
 def on_key_up(key):
     global score, game_over, move_list, current_move
     if key == keys.UP:
         update_dancer(0)
+        if move_list[current_move] == 0:
+            score = score + 1
+            next_move()
+        else:
+            game_over = True
     elif key == keys.RIGHT:
         update_dancer(1)
+        if move_list[current_move] == 1:
+            score = score+1
+            next_move()
+        else:
+            game_over = True
     elif key == keys.DOWN:
         update_dancer(2)
+        if move_list[current_move] == 2:
+            score = score+1
+            next_move()
+        else:
+            game_over = True
     elif key == keys.LEFT:
         update_dancer(3)
+        if move_list[current_move] == 3:
+            score = score + 1
+            next_move()
+        else:
+            game_over = True
 
+generate_moves()
+music.play("vanishing-horizon")
 
 def update():
-    pass
+    global say_dance, game_over, current_move, moves_complete
+    if moves_complete and not game_over:
+        generate_moves()
+        moves_complete = False
+        current_move = 0
+    elif game_over:
+        music.stop()
 
 pgzrun.go()
