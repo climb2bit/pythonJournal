a1bd185432d0654f0ccb594f13caa584399be89c
climb2bit <rhorizonus@hotmail.com>
2024-06-27 20:20:47 +0800

    3.PY'

--- nspbook1/2.py
+++ nspbook1/2.py
@@ -3,7 +3,7 @@
 import datetime, random
 
 def get_Birthdays(number_of_birthdays):
-    birthdays = []
+    birthdays = [] 
     for i in range(number_of_birthdays):
         start_of_year = datetime.date(2001,1,1)
         random_number_of_days = datetime.timedelta(random.randint(0,364))
