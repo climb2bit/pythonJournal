6d4082b58ed88b7e03828562b2348140208e881c
climb2bit <rhorizonus@hotmail.com>
2024-07-06 16:56:13 +0800

    5

---
+++ nspbook1/5.py
@@ -0,0 +1,136 @@
+'''DVD'''
+
+import sys, random, time
+
+try:
+   import bext
+except ImportError:
+   print('This program requires the bext module, which you')
+   print('can install by following the instructions at')
+   print('https://pypi.org/project/Bext/')
+
+WIDTH, HEIGHT = bext.size()
+WIDTH -= 1
+# WIDTH, HEIGHT = 20, 10
+
+# Actual run
+NUMBER_OF_LOGOS = 1
+# Debug
+# NUMBER_OF_LOGOS = 1
+PAUSE_AMOUNT = 0.2
+COLORS = ['red', 'green', 'blue', 'yellow', 'magenta', 'cyan', 'white']
+
+UP_RIGHT = 'ur'
+UP_LEFT = 'ul'
+DOWN_RIGHT = 'dr'
+DOWN_LEFT  = 'dl'
+DIRECTIONS = (UP_RIGHT, UP_LEFT,DOWN_RIGHT, DOWN_LEFT)
+
+COLOR = 'color'
+X = 'x'
+Y = 'y'
+DIR = 'direction'
+
+def main():
+    bext.clear()
+    logos = []
+    for i in range(NUMBER_OF_LOGOS):
+       logos.append({COLOR: random.choice(COLORS),
+                     X: random.randint(1, WIDTH - 4),
+                     Y: random.randint(1, HEIGHT - 4),
+                     DIR: random.choice(DIRECTIONS)})
+
+      # # DEBUG
+      #  logos.append({COLOR: random.choice(COLORS),
+      #                X: WIDTH - 3,
+      #                Y: 0,
+      #                DIR: UP_RIGHT})
+       if logos[-1][X] % 2 == 1:
+          logos[-1][X] -= 1
+    cornerBounces = 0
+    while True:
+      for logo in logos:
+          bext.goto(logo[X], logo[Y])
+          print('   ', end = '')
+          originalDirection = logo[DIR]
+
+          if logo[X] == 0 and logo[Y] == 0:
+            #  logo[DIR] == DOWN_RIGHT
+             logo[DIR] = DOWN_RIGHT
+             cornerBounces += 1
+          elif logo[X] == 0 and logo[Y] == HEIGHT - 1:
+            #  logo[DIR] == UP_RIGHT
+             logo[DIR] = UP_RIGHT
+             cornerBounces += 1
+          elif logo[X] == WIDTH - 3 and logo[Y] == 0:
+            #  logo[DIR] == DOWN_LEFT             
+             logo[DIR] = DOWN_LEFT             
+             cornerBounces += 1             
+          elif logo[X] == WIDTH - 3 and logo[Y] == HEIGHT - 1:
+            #  logo[DIR] == UP_LEFT
+             logo[DIR] = UP_LEFT
+             cornerBounces += 1       
+
+          elif logo[X] == 0 and logo[DIR] == UP_LEFT:
+             logo[DIR] = UP_RIGHT
+          elif logo[X] == 0 and logo[DIR] == DOWN_LEFT:              
+             logo[DIR] = DOWN_RIGHT
+          elif logo[X] == WIDTH - 3 and logo[DIR] == UP_RIGHT:
+             logo[DIR] = UP_LEFT
+          elif logo[X] == WIDTH - 3 and logo[DIR] == DOWN_RIGHT:
+             logo[DIR] = DOWN_LEFT  
+          elif logo[Y] == 0 and logo[DIR] == UP_LEFT:
+             logo[DIR] = DOWN_LEFT
+          elif logo[Y] == 0 and logo[DIR] == UP_RIGHT:
+             logo[DIR] = DOWN_RIGHT 
+          # suspect error here
+          elif logo[Y] == HEIGHT - 1 and logo[DIR] == DOWN_LEFT:
+          #  elif logo[Y] == HEIGHT - 3 and logo[DIR] == DOWN_LEFT:
+             logo[DIR] = UP_LEFT
+          # suspect error here
+          elif logo[Y] == HEIGHT - 1 and logo[DIR] == DOWN_RIGHT:
+         #  elif logo[Y] == WIDTH - 3 and logo[DIR] == DOWN_RIGHT:
+             logo[DIR] = UP_RIGHT
+          if logo[DIR] != originalDirection:
+              logo[COLOR] = random.choice(COLORS)
+          
+          if logo[DIR] == UP_RIGHT:
+                logo[X] += 2
+                logo[Y] -= 1
+          elif logo[DIR] == UP_LEFT:
+                logo[X] -= 2
+                logo[Y] -= 1
+          elif logo[DIR] == DOWN_RIGHT:
+                logo[X] += 2
+                logo[Y] += 1
+          elif logo[DIR] == DOWN_LEFT:
+                logo[X] -= 2
+                logo[Y] += 1
+
+      bext.goto(5,0)
+      bext.fg('white')
+      print('Corner bounces:', cornerBounces, end = '')
+
+      for logo in logos:
+         # to make sure the new position is within the valid range
+         # print (" ", logo[X], logo[Y], end = ' ')
+         logo[X] = min(WIDTH-3, logo[X])
+         logo[X] = max(0, logo[X])
+         logo[Y] = min(HEIGHT - 1, logo[Y])
+         logo[Y] = max(0, logo[Y])
+         # print (logo[X], logo[Y])
+         bext.goto(logo[X], logo[Y])
+         bext.fg(logo[COLOR])
+         print('DVD', end = '')
+         
+      bext.goto(0,0)
+      sys.stdout.flush()
+      time.sleep(PAUSE_AMOUNT)
+
+if __name__ == '__main__':
+   try:
+      main()
+   except KeyboardInterrupt:
+      print()
+      print('Bouncing DVD logo')
+      sys.exit()            
